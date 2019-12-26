0978e3ea33bc62cc21000adf048e566f8e71c75c
Nimra Awan <nimra@gmail.com>
2019-12-26 18:29:38 +0500

    commit

--- Ecommerce/myproject/src/structure.js
+++ Ecommerce/myproject/src/structure.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import HoverImage from "react-hover-image"
+import HoverImage from "react-hover-image";
+// import Details from './details';
 
 class Structure extends Component {
   render() {
@@ -10,30 +11,30 @@ class Structure extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              <HoverImage src={process.env.PUBLIC_URL + "Images/clothing.jpg"} alt = "ss" hoverSrc={process.env.PUBLIC_URL + "Images/clo_hover.jpg"} />
+                <HoverImage id="abc" src={process.env.PUBLIC_URL + "Images/Stitched/clothing.jpg"} alt="red" hoverSrc={process.env.PUBLIC_URL + "Images/Stitched/clo_hover.jpg"} />
             </div>
             <div className="col-md-4">
-              <img src={process.env.PUBLIC_URL + "Images/clothing.jpg"} alt="ss"  />
+              <HoverImage src={process.env.PUBLIC_URL + "Images/Stitched/black_front.jpg"} alt="black" hoverSrc={process.env.PUBLIC_URL + "Images/Stitched/black_back.jpg"} />
             </div>
             <div className="col-md-4">
-              <img src={process.env.PUBLIC_URL + "Images/clothing.jpg"} alt="ss"  />
+              <HoverImage src={process.env.PUBLIC_URL + "Images/Stitched/cream_front.jpg"} alt="ss" hoverSrc={process.env.PUBLIC_URL + "Images/Stitched/cream_back.jpg"} />
             </div>
           </div>
           <div className="row">
             <div className="col-md-4">
-                <img src={process.env.PUBLIC_URL + "Images/clothing.jpg"} alt="ss"  />
+              <HoverImage src={process.env.PUBLIC_URL + "Images/Stitched/peach_front.jpg"} alt="ss" hoverSrc={process.env.PUBLIC_URL + "Images/Stitched/peach_back.jpg"} />
             </div>
             <div className="col-md-4">
-              <img src={process.env.PUBLIC_URL + "Images/clothing.jpg"} alt="ss"  />
+              <HoverImage src={process.env.PUBLIC_URL + "Images/Stitched/purple_front.jpg"} alt="ss" hoverSrc={process.env.PUBLIC_URL + "Images/Stitched/purple_back.jpg"} />
             </div>
             <div className="col-md-4">
-              <img src={process.env.PUBLIC_URL + "Images/clothing.jpg"} alt="ss"  />
+              <HoverImage src={process.env.PUBLIC_URL + "Images/Stitched/clothing.jpg"} alt="ss" hoverSrc={process.env.PUBLIC_URL + "Images/Stitched/clo_hover.jpg"} />
             </div>
-            </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
-       
+
 export default Structure;
